ab2a3cd7fd83d4fa70e340e739f571307994e8a2
andrew.upk <andrew.upk@gmail.com>
2020-06-15 21:53:00 +0300

    Third commit: output config, generating html using plugin, clearing dist folder

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-  
+    const btn = document.createElement('button');
+
     element.innerHTML = _.join(['Andrew', 'Uschapovsky'], ' ');
     element.classList.add('hello');
 
     const myIcon = new Image();
     myIcon.src = Icon;
-
     element.appendChild(myIcon);
   
+    btn.innerHTML = 'Click me';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
   }
   
