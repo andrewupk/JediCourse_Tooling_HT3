38b69f2795be7d84908173cbe44b35424ef737d2
andrew.upk <andrew.upk@gmail.com>
2020-06-15 21:55:00 +0300

    Fourth commit: webpack-dev-server and HMR

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
  
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
         new HtmlWebpackPlugin({
             title: 'Output Management'
         })
